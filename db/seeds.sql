4cd5eede1e0d4acb2d88a67964fe2c4fac6564b1
mattesanna017 <matteo.mog.sanna@gmail.com>
2022-07-09 16:23:24 +0100

    sql foundation

---
+++ db/seeds.sql
@@ -0,0 +1,29 @@
+INSERT INTO department (name)
+VALUES  ("Inbound"),
+        ("Stock Control"),
+        ("Sortation");
+
+
+INSERT INTO employee_role (title, salary, department_id)
+VALUES      ("Section Manager", 27000, 11),
+            ("Team Leader", 22500, 21),
+            ("Admin", 18500, 31);
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES      ("Edoardo", "Secreto", 8941, 6 ),
+            ("Peter", "Nagy", 8942, 7),
+            ("Weronika", "Borownska", 8951, NULL),
+            ("Estefany", "Murillo", 8952, NULL),
+            ("Dominik", "Kranoski", 8953, NULL)
+            ("Marco", "Vieira", 8961, NULL),
+            ("Fiori", "Zemikal", 8962, NULL)
+            ("Jonh", "Disab", 8963, NULL),
+            ("Alexandra", "Pantilescu", 8964, NULL),
+            ("Raffaele", "De Luca", 8965, NULL)
+
+
+
+
+
+
