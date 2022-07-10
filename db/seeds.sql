c0d8b2edeb2c76858fe4b3411df7071f093b82b1
mattesanna017 <matteo.mog.sanna@gmail.com>
2022-07-10 23:49:45 +0100

    packaqes foundation

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,31 +1,17 @@
 INSERT INTO department (department_name)
-VALUES  ('Inbound'),
-        ('Stock Control'),
-        ('Sortation');
+VALUES ("Inbound"),
+       ("Stock Control");
 
-
+  
 INSERT INTO employee_role (title, salary, department_id)
-VALUES      ('Section Manager', 27000, 11),
-            ('Team Leader', 22500, 11),
-            ('Admin', 18500, 11);
+VALUES ("Section Manager", 28000,1),
+       ("Team Leader", 25000,1);
+       
 
+       
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES      ('Edoardo', 'Secreto', 22, 5 ),
-            ('Peter', 'Nagy', 22, 5 ),
-            ('Patricia', 'Germanova', 22, 5),
-            ('Weronika', 'Borowanska', 22, NULL),
-            ('Estefany', 'Murillo', 22, NULL),
-            ('Dominik', 'Kranoski', 22, NULL),
-            ('Marco', 'Vieira', 22, NULL),
-            ('Fiori', 'Zemikal', 22, NULL),
-            ('Jonh', 'Disab', 22, NULL),
-            ('Alexandra', 'Pantilescu', 22, NULL),
-            ('Raffaele', 'De Luca', 22, NULL),
-            ('Kasete', 'Goitom', 22, NULL);
-
-
-
-
-
-
+INSERT INTO employee (first_name, last_name, role_id, manager_id)     
+VALUES ("Kasete", "Goitom", 2, NULL),
+       ("Edoardo", "Secreto", 2, 1),
+       ("James", "Barnes", 2, NULL);
+    
