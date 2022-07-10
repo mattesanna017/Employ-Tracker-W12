fd50db42104cd43896e9086b4c253d426b962bfd
mattesanna017 <matteo.mog.sanna@gmail.com>
2022-07-10 22:46:55 +0100

    seeds finally working :)

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,28 +1,28 @@
 INSERT INTO department (department_name)
-VALUES  ("Inbound"),
-        ("Stock Control"),
-        ("Sortation");
+VALUES  ('Inbound'),
+        ('Stock Control'),
+        ('Sortation');
 
 
 INSERT INTO employee_role (title, salary, department_id)
-VALUES      ("Section Manager", 27000, 11),
-            ("Team Leader", 22500, 21),
-            ("Admin", 18500, 31);
+VALUES      ('Section Manager', 27000, 11),
+            ('Team Leader', 22500, 11),
+            ('Admin', 18500, 11);
 
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES      ("Edoardo", "Secreto", 8941, 6 ),
-            ("Peter", "Nagy", 8942, 7),
-            ("Patricia", "Germanova", 8943, 8)
-            ("Weronika", "Borowanska", 8951, NULL),
-            ("Estefany", "Murillo", 8952, NULL),
-            ("Dominik", "Kranoski", 8953, NULL)
-            ("Marco", "Vieira", 8961, NULL),
-            ("Fiori", "Zemikal", 8962, NULL)
-            ("Jonh", "Disab", 8963, NULL),
-            ("Alexandra", "Pantilescu", 8964, NULL),
-            ("Raffaele", "De Luca", 8965, NULL),
-            ("Kasete", "Goitom", 8966, NULL);
+VALUES      ('Edoardo', 'Secreto', 22, 5 ),
+            ('Peter', 'Nagy', 22, 5 ),
+            ('Patricia', 'Germanova', 22, 5),
+            ('Weronika', 'Borowanska', 22, NULL),
+            ('Estefany', 'Murillo', 22, NULL),
+            ('Dominik', 'Kranoski', 22, NULL),
+            ('Marco', 'Vieira', 22, NULL),
+            ('Fiori', 'Zemikal', 22, NULL),
+            ('Jonh', 'Disab', 22, NULL),
+            ('Alexandra', 'Pantilescu', 22, NULL),
+            ('Raffaele', 'De Luca', 22, NULL),
+            ('Kasete', 'Goitom', 22, NULL);
 
 
 
