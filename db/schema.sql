e73175cbb29dd6d2c4ead2cdcbb2c7f2feb6fd24
mattesanna017 <matteo.mog.sanna@gmail.com>
2022-07-10 13:11:25 +0100

    minor update

--- db/schema.sql
+++ db/schema.sql
@@ -1,18 +1,18 @@
-DROP DATABASE IF EXISTS company_tracker;
-CREATE DATABASE company_tracker;
+DROP DATABASE IF EXISTS companyTracker_db;
+CREATE DATABASE companyTracker_db;
 
-USE company_tracker;
+USE companyTracker_db;
 
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL
+    department_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE employee_role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT NOT NULL
+    department_id INT NOT NULL,
 
     FOREIGN KEY (department_id)
     REFERENCES department(id)
@@ -25,7 +25,7 @@ CREATE TABLE employee (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    manager_id INT 
+    manager_id INT,
 
     FOREIGN KEY (role_id)
     REFERENCES employee_role(id)
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO department (name)
+INSERT INTO department (department_name)
 VALUES  ("Inbound"),
         ("Stock Control"),
         ("Sortation");
@@ -13,14 +13,16 @@ VALUES      ("Section Manager", 27000, 11),
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES      ("Edoardo", "Secreto", 8941, 6 ),
             ("Peter", "Nagy", 8942, 7),
-            ("Weronika", "Borownska", 8951, NULL),
+            ("Patricia", "Germanova", 8943, 8)
+            ("Weronika", "Borowanska", 8951, NULL),
             ("Estefany", "Murillo", 8952, NULL),
             ("Dominik", "Kranoski", 8953, NULL)
             ("Marco", "Vieira", 8961, NULL),
             ("Fiori", "Zemikal", 8962, NULL)
             ("Jonh", "Disab", 8963, NULL),
             ("Alexandra", "Pantilescu", 8964, NULL),
-            ("Raffaele", "De Luca", 8965, NULL)
+            ("Raffaele", "De Luca", 8965, NULL),
+            ("Kasete", "Goitom", 8966, NULL);
 
 
 
